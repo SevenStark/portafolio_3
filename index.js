d5f1d63a947466e55fc231d353a2e16cab7e274f
SevenStark <7s.nation1@gmail.com>
2024-01-04 15:57:44 -0500

    Version 3

--- index.js
+++ index.js
@@ -6,11 +6,27 @@ const skill1 = document.getElementById('skill1');
 const skill2 = document.getElementById('skill2');
 const dipyexp = document.getElementById('dye');
 
+titulo.textContent = "DESCRIPCION";
+        descripcion.textContent = "Soy desarollardor web full stack en formacion con experiencia en la creacion de paginas web resposivas, estaticas y dinamicas que ayudan a las empresas a mostrar sus productos y retribuir su inversion en TI ";
+        skill1.textContent = "-C++ | Aplicaion de delivery";
+        skill2.textContent = "- ARDUINO | Manejo de actuadores de hardware";
+        dipyexp.textContent = "CERTIFICACIONES Y EXPERIENCIAS";
+
 idiomaActual.addEventListener('click',function(){
     if(idiomaActual.textContent == "ENGLISH"){
         idiomaActual.textContent = "ESPAÑOL";
+        titulo.textContent = "DESCRIPTION";
+        descripcion.textContent = "I am a full stack web developer in training with experience in creating responsive, static and dynamic web pages that help companies show their products and return their investment in IT";
+        skill1.textContent = "-C++ | Delivery application";
+        skill2.textContent = "- ARDUINO | Handling hardware actuators";
+        dipyexp.textContent = "CERTIFICATIONS AND EXPERIENCES";
     }else{
         idiomaActual.textContent = "ENGLISH";
+        titulo.textContent = "DESCRIPCION";
+        descripcion.textContent = "Soy desarollardor web full stack en formacion con experiencia en la creacion de paginas web resposivas, estaticas y dinamicas que ayudan a las empresas a mostrar sus productos y retribuir su inversion en TI ";
+        skill1.textContent = "-C++ | Aplicaion de delivery";
+        skill2.textContent = "- ARDUINO | Manejo de actuadores de hardware";
+        dipyexp.textContent = "CERTIFICACIONES Y EXPERIENCIAS";
     }
 
 });
